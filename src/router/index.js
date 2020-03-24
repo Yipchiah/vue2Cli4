750e22c6edcf7ab36cd7f5249d11d7937fe49cae
xwz <zsgp19941111@163.com>
2020-03-24 08:35:12 +0800

    delete skeleton

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/about',
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,6 @@
-const {
-  SkeletonPlugin
-} = require('page-skeleton-webpack-plugin')
-const path = require('path')
 module.exports = {
   publicPath: './',
   productionSourceMap: false,
-  // webpack 相关配置
-  configureWebpack: {
-    plugins: [
-      new SkeletonPlugin({
-        pathname: path.resolve(__dirname, './shell'), // the path to store shell file
-        staticDir: path.resolve(__dirname, './dist'), // the same as the `output.path`
-        routes: ['/', '/about'] // Which routes you want to generate skeleton screen
-      })
-    ]
-  },
   css: {
     loaderOptions: {
       sass: {
